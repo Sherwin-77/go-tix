3827d9114c9d9e5eaf6d1d16d2617d7adf066b40
sherwin-77 <sherwinak1@gmail.com>
2024-12-10 20:33:06 +0700

    refine(notification): Adjust migration file

--- db/migrations/20241208145520_create_notifications_table.up.sql
+++ db/migrations/20241208145520_create_notifications_table.up.sql
@@ -3,10 +3,11 @@ BEGIN;
 CREATE TABLE notifications (
     id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
-    triggerable VARCHAR(50),
-    read_at DATE,
+    triggerable_id UUID NOT NULL,
+    triggerable_type VARCHAR(255) NOT NULL,
+    read_at TIMESTAMP(6) WITH TIME ZONE,
     created_at TIMESTAMP(6) WITH TIME ZONE,
-    metadata JSON,
+    metadata JSONB,
 
     CONSTRAINT notifications_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
 );
