9ea598ddc75798f3de08c3318e51d154e354e133
TimesCoder <joshuabulyan24@gmail.com>
2024-12-13 23:19:01 +0700

    update

--- internal/entity/event_approval.go
+++ internal/entity/event_approval.go
@@ -19,8 +19,8 @@ type EventApproval struct {
 	Description string                                    `json:"description" gorm:"type:varchar(2047)"`
 	Organizer   string                                    `json:"organizer" gorm:"type:varchar(255);not null"`
 	Location    string                                    `json:"location" gorm:"type:varchar(2047);not null"`
-	Longitude   float64                                   `json:"longitude" gorm:"type:decimal(12,8)"`
-	Latitude    float64                                   `json:"latitude" gorm:"type:decimal(12,8)"`
+	Longitude   float64                                   `json:"longitude" gorm:"type:decimal(8,6)"`
+	Latitude    float64                                   `json:"latitude" gorm:"type:decimal(9,6)"`
 	StartAt     time.Time                                 `json:"start_at" gorm:"type:timestamp(6) with time zone;not null"`
 	EndAt       time.Time                                 `json:"end_at" gorm:"type:timestamp(6) with time zone;not null"`
 	CreatedAt   time.Time                                 `json:"created_at" gorm:"type:timestamp(6) with time zone;not null"`
