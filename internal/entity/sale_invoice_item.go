828e459d54c67efdfd06ec18398354c87015cdea
sherwin-77 <sherwinak1@gmail.com>
2024-12-18 00:51:45 +0700

    fix(sale-invoice-item): Add missing price field

--- internal/entity/sale_invoice_item.go
+++ internal/entity/sale_invoice_item.go
@@ -13,6 +13,7 @@ type SaleInvoiceItem struct {
 	SaleInvoiceID   uuid.UUID                                   `json:"sale_invoice_id" gorm:"type:uuid;not null"`
 	InvoiceableID   uuid.UUID                                   `json:"invoiceable_id" gorm:"type:uuid;not null"`
 	InvoiceableType string                                      `json:"invoiceable_type" gorm:"type:varchar(255);not null"`
+	Price           float64                                     `json:"price" gorm:"type:decimal(12,2);not null"`
 	Qty             int                                         `json:"qty" gorm:"type:integer;not null"`
 	Total           float64                                     `json:"total" gorm:"type:decimal(16,2);not null"`
 	Metadata        datatypes.JSONType[SaleInvoiceItemMetadata] `json:"metadata" gorm:"type:jsonb"`
